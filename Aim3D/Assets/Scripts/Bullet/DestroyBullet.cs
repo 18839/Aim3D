3a7f585e91be12eecddea98169e9e77f65d9e912
18839 <18839@ma-web.nl>
2015-12-10 23:28:35 +0100

    Git Update 10-12-15

--- Aim3D/Assets/Scripts/Bullet/DestroyBullet.cs
+++ Aim3D/Assets/Scripts/Bullet/DestroyBullet.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class DestroyBullet : MonoBehaviour {
+    //Vector3
+    private Vector3 _myRotation;
+    //Vector3
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    //GameObject
+    private GameObject _getPlayerRotation;
+    //GameObject
+
+    void Start()
+    {
+        _getPlayerRotation = GameObject.FindGameObjectWithTag("Player");
+        _myRotation = _getPlayerRotation.transform.TransformDirection(Vector3.back);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "ReflectWall")
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(_myRotation * 100, ForceMode.Impulse);
+        }
+    }
 }
